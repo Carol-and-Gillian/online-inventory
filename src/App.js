7ce339cf006fec2b9e28131fcf95c24ceaa8a8c4
gillianyue <gillianyue1999@hotmail.com>
2017-01-26 15:00:50 -0500

    Links to Images!!!!!!!!!!!
    and other pages.

--- src/App.js
+++ src/App.js
@@ -18,4 +18,17 @@ class App extends Component {
   }
 }
 
+export function ActionLink() {
+    function handleClick(e) {
+        e.preventDefault();
+        console.log('The link was clicked.');
+    }
+
+    return (
+        <a href="#" onClick={handleClick}>
+            Click me
+        </a>
+    );
+}
+
 export default App;
