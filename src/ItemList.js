7e65edcdc34722e0ad38797ad1552cab2afd3b19
gillianyue <gillianyue1999@hotmail.com>
2017-02-06 09:22:51 -0500

    ?
    ???

--- src/ItemList.js
+++ src/ItemList.js
@@ -4,19 +4,21 @@
 import React, { Component } from 'react';
 
 
-class itemPlaceHolder extends Component{
-    render(){
+var itemPlaceHolder = React.createClass({
+    render: function () {
+
     return(
 <div className="row placeholders">
     <div className="col-xs-6 col-sm-3 placeholder">
         <img src="http://cdn.shopify.com/s/files/1/0277/0693/products/apparel-cosmos-tee-1_large.jpg?v=1480339965"
-             width="200" height="200" className="img-responsive" alt="" />
-            <h4>Item</h4>
-            <span className="text-muted">Short Description for Item</span>
+             width="200" height="200" className="img-responsive" alt=""/>
+        <h4>itemPlaceHolderYay</h4>
+        <span className="text-muted">ItemPlaceHolderDescription</span>
     </div>
 </div>
-        );
-        }
-        }
+)
+}
+        });
 
+    ReactDOM.render(<itemPlaceHolder/>, document.getElementsByID('itemPlaceHolder1'));
         export default itemPlaceHolder;
