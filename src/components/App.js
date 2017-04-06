bc5f36243902d394530d321c9aa5fd7aa66e784f
gillianyue <gillianyue1999@hotmail.com>
2017-04-06 16:21:36 -0400

    update on three

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import { Navbar, NavItem, NavDropdown, MenuItem, Nav} from 'react-bootstrap';
 import {LinkContainer} from "react-router-bootstrap";
-import ThreeTest from './ThreeTest';
+import Simple from './miniTest';
 
 import MainPageComponents from "./MainPage";
 import '../../src/CSS/Style.css';
@@ -41,7 +41,7 @@ export default class App extends Component {
 
             {(this.props.children==null ? <MainPageComponents/> : this.props.children)}
 
-            <ThreeTest/>
+            <Simple width={400} height={300} />
 
         </div>
     );
